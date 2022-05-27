fb8f9ffa1bc409dde3db8148c2fc85c27feb2c9b
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2022-05-27 15:53:50 -0400

    Add order

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp3
         static void Main(string[] args)
         {
             List<int> list1 = new List<int> { 1, 2, 10, 3, 4, 9, 5, 6, 8, 7, 8, 9, 10 };
-            List<int> list2 = list1.Distinct().ToList();
+            List<int> list2 = list1.OrderBy(x => x).Distinct().ToList();
             foreach (var item in list2)
             {
                 Console.WriteLine(item);
