464399bdea6dd2b870f758eff8c92082eb6061bb
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2022-05-27 15:45:50 -0400

    App 3

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list1 = new List<int> { 1, 2, 10, 3, 4, 9, 5, 6, 8, 7, 8, 9, 10 };
+            List<int> list2 = list1.Distinct().ToList();
+            foreach (var item in list2)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
