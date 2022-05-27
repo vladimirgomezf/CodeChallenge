50843aa8706f81fe3dd105d0279dd060209d4255
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2022-05-27 15:31:30 -0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre una frase: ");
+            var frase = Console.ReadLine();
+
+            Char[] temp = frase.Trim().ToCharArray();
+            Array.Sort(temp);
+            var tempo = new String(temp).Trim();
+
+            String final = "";
+            int j = 0;
+            for (int i = 0; i < frase.Length; i++)
+            {
+                if (String.Equals(frase[i].ToString()," "))
+                {
+                    final = final + " ";
+                }else
+                {
+                    final = final + tempo[j++].ToString();
+                }
+            }
+
+            Console.WriteLine(final);
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        private float salary(DateTime hora_inicio, DateTime hora_fin, float salario_hora, float factor_multiplicador)
+        {
+            if (hora_inicio.Hour > 8 && hora_fin.Hour < 18)
+            {
+                float total_horas = hora_fin.Hour - hora_inicio.Hour;
+                return total_horas * salario_hora;
+            }else if(hora_inicio.Hour < 8 && hora_fin.Hour < 18)
+            {
+                var normal = hora_fin.Hour - 8;
+                var extra = 8 - hora_inicio.Hour;
+                return normal * salario_hora + extra * factor_multiplicador;
+            }else if(hora_inicio.Hour > 8 && hora_fin.Hour > 18)
+            {
+                var normal = 18 - hora_inicio.Hour;
+                var extra = hora_fin.Hour - 18;
+                return normal* salario_hora + extra* factor_multiplicador;
+            }else
+            {
+                var extra = (8 - hora_inicio.Hour) + (hora_fin.Hour - 18) ;
+                return (18-8) * salario_hora + extra * factor_multiplicador;
+            }
+        }
+    }
+}
