80e32aa2c0a32b8672096ea49c9a0cf4c07c6b08
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2022-05-27 18:33:16 -0400

    Fix Ex. 3

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -8,11 +8,35 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            List<int> list1 = new List<int> { 1, 2, 10, 3, 4, 9, 5, 6, 8, 7, 8, 9, 10 };
-            List<int> list2 = list1.OrderBy(x => x).Distinct().ToList();
-            foreach (var item in list2)
+            List<int> list1 = new List<int> { 1, 2, 10, 3, 4, 9, 5, 6, 8, 7, 8, 9, 10 };//correct output
+            removeDuplicate(list1);
+            Console.WriteLine();
+            list1 = new List<int> { 1, 2, 10, 3, 4, 9, 5, 6, 8, 7, 8, 9 };//wrong output, check example
+            removeDuplicate(list1);
+        }
+        static void removeDuplicate(List<int> list1)
+        {
+            foreach (var item in list1)
             {
-                Console.WriteLine(item);
+                Console.Write(item + ",");
+            }
+            Console.WriteLine();
+
+            for (int i = list1.Count-1; i >= 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (list1[i] == list1[j])
+                    {
+                        list1.RemoveAt(j);
+                        i--;
+                    }
+                }
+            }
+
+            foreach (var item in list1)
+            {
+                Console.Write(item + ",");
             }
         }
     }
