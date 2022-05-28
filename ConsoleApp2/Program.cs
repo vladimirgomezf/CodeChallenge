096b4afa79d0e4c93d2a8b081ae1be09a9d7b974
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2022-05-27 20:14:24 -0400

    Fix Ex. 2

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -7,25 +7,63 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
 
+            //DateTime inicio = new DateTime(2022, 5, 26, 8, 0, 0);
+            //DateTime fin = new DateTime(2022, 5, 26, 18, 0, 0);
+            //var result = salary(inicio, fin, 1, 2);
+
+            //DateTime inicio = new DateTime(2022, 5, 26, 6, 0, 0);
+            //DateTime fin = new DateTime(2022, 5, 26, 18, 0, 0);
+            //var result = ConsoleApp2.Program.salary(inicio, fin, 1, 2);
+
+            //DateTime inicio = new DateTime(2022, 5, 26, 6, 0, 0);
+            //DateTime fin = new DateTime(2022, 5, 26, 20, 0, 0);
+            //var result = ConsoleApp2.Program.salary(inicio, fin, 1, 2);
+
+            //Este no
+            //DateTime inicio = new DateTime(2022, 5, 26, 4, 0, 0);
+            //DateTime fin = new DateTime(2022, 5, 26, 6, 0, 0);
+            //var result = ConsoleApp2.Program.salary(inicio, fin, 1, 2);
+
+            //DateTime inicio = new DateTime(2022, 5, 26, 19, 0, 0);
+            //DateTime fin = new DateTime(2022, 5, 26, 21, 0, 0);
+            //var result = ConsoleApp2.Program.salary(inicio, fin, 1, 2);
+
+            //DateTime inicio = new DateTime(2022, 5, 26, 12, 0, 0);
+            //DateTime fin = new DateTime(2022, 5, 26, 14, 0, 0);
+            //var result = ConsoleApp2.Program.salary(inicio, fin, 1, 2);
+
+
+            DateTime inicio = new DateTime(2022, 5, 26, 12, 0, 0);
+            DateTime fin = new DateTime(2022, 5, 26, 20, 0, 0);
+            var result = ConsoleApp2.Program.salary(inicio, fin, 1, 2);
+            Console.WriteLine(result);
         }
 
-        private float salary(DateTime hora_inicio, DateTime hora_fin, float salario_hora, float factor_multiplicador)
+        static float salary(DateTime hora_inicio, DateTime hora_fin, float salario_hora, float factor_multiplicador)
         {
-            if (hora_inicio.Hour > 8 && hora_fin.Hour < 18)
+            if (hora_inicio.Hour >= 8 && hora_fin.Hour <= 18)
             {
                 float total_horas = hora_fin.Hour - hora_inicio.Hour;
                 return total_horas * salario_hora;
-            }else if(hora_inicio.Hour < 8 && hora_fin.Hour < 18)
+            }
+            else if ((hora_inicio.Hour < 8 && hora_fin.Hour < 8) || (hora_inicio.Hour > 18 && hora_fin.Hour > 18))
+            {
+                float total_horas = hora_fin.Hour - hora_inicio.Hour;
+                return total_horas * factor_multiplicador;
+            }
+            else if(hora_inicio.Hour < 8 && hora_fin.Hour <= 18)
             {
                 var normal = hora_fin.Hour - 8;
                 var extra = 8 - hora_inicio.Hour;
                 return normal * salario_hora + extra * factor_multiplicador;
-            }else if(hora_inicio.Hour > 8 && hora_fin.Hour > 18)
+            }
+            else if(hora_inicio.Hour >= 8 && hora_fin.Hour > 18)
             {
                 var normal = 18 - hora_inicio.Hour;
                 var extra = hora_fin.Hour - 18;
                 return normal* salario_hora + extra* factor_multiplicador;
-            }else
+            }
+            else
             {
                 var extra = (8 - hora_inicio.Hour) + (hora_fin.Hour - 18) ;
                 return (18-8) * salario_hora + extra * factor_multiplicador;
