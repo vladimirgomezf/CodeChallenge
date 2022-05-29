fc11ca2bcb0d271d0123556a1d1b10f23057aee9
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2022-05-29 12:07:51 -0400

    Add UserModel class

---
+++ BugsWebAPI/Models/UserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BugsWebAPI.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
