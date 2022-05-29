6849c5d15da8e859188cdefef70ac006daa52d48
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2022-05-29 17:58:25 -0400

    Create and initialize the data context

---
+++ BugsWebAPI/Data/BugsContext.cs
@@ -0,0 +1,24 @@
+﻿using BugsWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BugsWebAPI.Data
+{
+    public class BugsContext: DbContext
+    {
+        public BugsContext(DbContextOptions options): base(options)
+        {
+
+        }
+
+        public DbSet<UserModel> UserModels { get; set; }
+        public DbSet<ProjectModel> ProjectModels { get; set; }
+        public DbSet<BugModel> BugModels { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserModel>().ToTable("Users");
+            modelBuilder.Entity<ProjectModel>().ToTable("Projects");
+            modelBuilder.Entity<BugModel>().ToTable("Bugs");
+        }
+    }
+}
--- BugsWebAPI/Startup.cs
+++ BugsWebAPI/Startup.cs
@@ -1,7 +1,9 @@
+using BugsWebAPI.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,7 +28,8 @@ namespace BugsWebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            string ConnectionString = Configuration.GetConnectionString("Local");
+            services.AddDbContext<BugsContext>(opt => opt.UseSqlServer(ConnectionString));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
