34354c2cc71861f6f6ede47b993a8217de23a3bd
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2022-06-01 03:33:39 -0400

    Inject the dependencies

--- BugsFrontApp/Program.cs
+++ BugsFrontApp/Program.cs
@@ -1,3 +1,5 @@
+using BugsFrontApp.Services;
+using BugsFrontApp.Services.Implements;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -19,6 +21,9 @@ namespace BugsFrontApp
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+            builder.Services.AddScoped<IUserService, UserService>();
+            builder.Services.AddScoped<IProjectService, ProjectService>();
+            builder.Services.AddScoped<IBugService, BugService>();
             await builder.Build().RunAsync();
         }
     }
