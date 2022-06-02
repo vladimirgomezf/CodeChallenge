d2da971124bd82dcde95af32b13215643e7cd334
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2022-06-02 19:51:10 -0400

    Add UserDTO class

---
+++ BugsWebAPI/DTOs/UserDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace BugsWebAPI.DTOs
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
